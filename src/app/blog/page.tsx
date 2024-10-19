65027b206b1a381a5b14d018a76d8e6374537e2b
hosedraggersinc <hosedraggersinc@outlook.com>
2024-10-19 11:55:27 -0400

    Add files via upload

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,22 +4,23 @@ import NavBar from "../components/NavBar";
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-1 w-full min-h-screen">
+    <div className="w-full min-h-screen">
       <main className="relative flex flex-col w-full min-h-screen">
-        {/* Nested Grid */}
-        <div className="hero flex flex-col items-center justify-center py-10">
-          {/* Navigation Menu */}
-          <div className="absolute top-0 left-0 w-full">
-            <NavBar />
-          </div>
-          <div className="uppercase text-3xl text-zinc-300">
+        {/* Navigation Menu */}
+        <div className="absolute top-0 left-0 w-full">
+          <NavBar />
+        </div>
+
+        {/* Hero Section */}
+        <div className="hero flex flex-col items-center justify-center py-10 text-center">
+          <h1 className="uppercase text-3xl md:text-4xl text-zinc-300">
             Hosebed Chronicles: Exploring the World of Quarterbacking
-          </div>
+          </h1>
           <div>
             <a href="#readmore">
               <button
                 type="button"
-                className="mt-4 px-4 py-2 rounded bg-gradient-to-r from-zinc-500 to-slate-500 hover:from-pink-500 hover:to-rose-500"
+                className="mt-4 px-6 py-2 rounded bg-gradient-to-r from-zinc-500 to-slate-500 hover:from-pink-500 hover:to-rose-500 transition duration-300"
               >
                 Read More
               </button>
@@ -27,8 +28,9 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="flex flex-col p-10 w-full h-full">
-          <div className="relative pr-28 grid grid-cols-3 gap-6 w-full h-full">
+        {/* Blog Thumbnails Section */}
+        <div className="flex flex-col p-4 w-full h-full">
+          <div className="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full h-full">
             <div className="flex flex-col items-center justify-start">
               <div className="pt-4" id="readmore"></div>
               <VentBlogThumbnail />
